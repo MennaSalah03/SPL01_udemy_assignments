f2a4cfb56b1c649b85166dca6eb32fbb4042c65e
mennasalah <menna.skh2003@gmail.com>
2025-04-24 00:15:12 +0200

    MODIFY: The code for echo with failed tests..

--- UNIX_utilities/echo.c
+++ UNIX_utilities/echo.c
@@ -2,11 +2,30 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <fcntl.h>
 
 
 int main(int argc, char **argv)
 {
-    
-
+    if (argc == 1)
+    {
+        exit(-1);
+    }
+    char nxt_scp = '\0';
+    for (int i = 1; i < argc; i++)
+    {
+        for (int j = 0; j < strlen(argv[i]); j++)
+        {
+            if(argv[i][j] == '/')
+            {
+                nxt_scp = argv[i][j + 1];
+            }
+        }
+        write(1, argv[i], strlen(argv[i]));
+        if (i != argc - 1)
+        {
+            write(1, " ", 1);
+        }
+    }
     return 0;
 }
